a8051ff04aeb0a6130c932afb0f3b188ef6eaeee
Mangaraj Das <166250212+Mangarj@users.noreply.github.com>
2026-03-14 22:31:25 +0530

    Time: 7 ms (21.14%) | Memory: 23.8 MB (74.96%) - LeetSync

---
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.py
@@ -0,0 +1,8 @@
+
+class Solution:
+    def countNodes(self, root):
+        if not root:
+            return 0
+        
+        return 1 + self.countNodes(root.left) + self.countNodes(root.right)
+        
